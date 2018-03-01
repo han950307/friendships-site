ec4024cf4afce85e27c55dda3cf83b1334993cac
Hansung Kang <h.k@duke.edu>
2018-03-01 10:30:45 -0500

    trying templates

--- friendship/views.py
+++ friendship/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from django.conf import settings
 from .models import UserInfo
 
 
 # Create your views here.
 def index(request):
-    print(settings.AUTH_USER_MODEL)
-    user_list = UserInfo.objects.all().select_related()
-    output = ', '.join([q.user.email for q in user_list])
-    return HttpResponse(output)
+    user_list = UserInfo.objects.all()
+    template = loader.get_template('index.html')
+    context = {
+        'user_list': user_list,
+    }
+    return HttpResponse(template.render(context, request))
