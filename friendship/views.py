24a17255b8889fd2e6786a1b831b4adb8667d832
Hansung Kang <h.k@duke.edu>
2018-03-01 01:51:06 -0500

    trying user

--- friendship/views.py
+++ friendship/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth import User
-from .models import UserInfo
+from django.conf import settings
 
 
 # Create your views here.
 def index(request):
-    user_list = UserInfo.objects.all().select_related()
+    user_list = settings.AUTH_USER_MODEL.objects.all().select_related()
     output = ', '.join([q.email for q in user_list])
     return HttpResponse(output)
