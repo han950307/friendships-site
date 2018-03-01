655ab771f02e39d8f8f6d3f6a7959be3fa356b42
Hansung Kang <h.k@duke.edu>
2018-03-01 00:02:39 -0500

    added database schema

--- friendsite/settings.py
+++ friendsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["0.0.0.0", "52.14.138.237"]
 # Application definition
 
 INSTALLED_APPS = [
+    'frontend.apps.FrontendConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class UserInfo(models.Model):
+    """
+    Links to the User model.
+    """
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    shipping_address = models.CharField(max_length=500)
+    phone = models.CharField(max_length=25)
+    is_receiver = models.BooleanField(default=True)
+    is_sender = models.BooleanField(default=False)
+
+
+class Order(models.Model):
+    """
+    An entry gets created when a receiver places an order.
+    """
+    url = models.URLField()
+    merchandise_type = models.IntegerField()
+    # When the receiver places a request
+    date_placed = models.DateTimeField(auto_now_add=True)
+    # When the sender accepts the request
+    date_accepted = models.DateTimeField(null=True)
+    # When the receiver agrees to the weight and size & money
+    date_verified = models.DateTimeField(null=True)
+    # When it was delivered
+    date_completed = models.DateTimeField(null=True)
+    status = models.IntegerField()
+    sender_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    receiver_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+
+class Images(models.model):
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+    user_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    order_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    image = models.ImageField()
+    mimetype = models.CharField(max_length=25)
+    # Bank-slip or whatever
+    image_type = models.IntegerField()
