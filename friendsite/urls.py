20d6d698294fdd840365429b595fc645ac846c27
Hansung Kang <h.k@duke.edu>
2018-03-01 10:55:58 -0500

    trying templates

--- friendsite/urls.py
+++ friendsite/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('friendship/', include('friendship.urls'))
+    path('', include('friendship.urls'))
 ]
