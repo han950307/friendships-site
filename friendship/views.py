096ec7967b8fe3fab07d39df28b1737c0dae547f
Hansung Kang <h.k@duke.edu>
2018-03-01 01:39:43 -0500

    added adminsite stuff

--- friendship/views.py
+++ friendship/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import User
+
 
 # Create your views here.
+def index(request):
+    user_list = User.objects.all()
+    output = ', '.join([q.email for q in user_list])
+    return HttpResponse(output)
