2c254fb279486f668f71786d2257d2e1ec37b5b9
Hansung Kang <h.k@duke.edu>
2018-03-01 10:48:54 -0500

    trying templates

--- friendship/views.py
+++ friendship/views.py
@@ -3,13 +3,15 @@ from django.http import HttpResponse
 from django.template import loader
 from django.conf import settings
 from .models import UserInfo
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 
 # Create your views here.
 def index(request):
     user_list = UserInfo.objects.all()
-    template = loader.get_template('friendship/index.html')
-    context = {
-        'user_list': user_list,
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'friendship/index.html', {'user_list': user_list})
+
+def details(request, user_id):
+    user = get_object_or_404(UserInfo, pk=user_id)
+    return render(request, 'friendship/details.html', {'user_info': user})
