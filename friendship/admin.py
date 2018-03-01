d645638f6ef76ab8c1771ce6f6f46d40a4651310
Hansung Kang <h.k@duke.edu>
2018-03-01 00:32:31 -0500

    added adminsite stuff

--- friendship/admin.py
+++ friendship/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import Image
-from .models import User
+from .models import UserInfo
 from .models import Order
 
 
 # Register your models here.
 admin.site.register(Image)
 admin.site.register(Order)
-admin.site.register(User)
+admin.site.register(UserInfo)
