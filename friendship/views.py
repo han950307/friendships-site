5dd62da098a449d532a2e445fea03dbcdcfa567f
Hansung Kang <h.k@duke.edu>
2018-03-01 02:00:14 -0500

    currently debugging views

--- friendship/views.py
+++ friendship/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
+from .models import UserInfo
 
 
 # Create your views here.
 def index(request):
-    user_list = settings.AUTH_USER_MODEL.objects.all()
-    print(user_list)
+    print(settings.AUTH_USER_MODEL)
+    user_list = UserInfo.objects.all().select_related()
     output = ', '.join([q.email for q in user_list])
     return HttpResponse(output)
--- friendsite/urls.py
+++ friendsite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('friendship/', include('friendship.urls')),
     path('admin/', admin.site.urls),
+    path('', include('friendship.urls'))
 ]
