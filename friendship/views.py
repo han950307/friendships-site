ddae6cec611525dfd4039296c928ef2f2f671fae
Hansung Kang <h.k@duke.edu>
2018-03-01 01:49:59 -0500

    added views

--- friendship/views.py
+++ friendship/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import User
+from django.contrib.auth import User
+from .models import UserInfo
 
 
 # Create your views here.
 def index(request):
-    user_list = User.objects.all()
+    user_list = UserInfo.objects.all().select_related()
     output = ', '.join([q.email for q in user_list])
     return HttpResponse(output)
