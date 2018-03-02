8d68addb548f063e6b35fb9b1eef1a728cc557c1
Hansung Kang <h.k@duke.edu>
2018-03-01 19:59:52 -0500

    registration trial

--- friendship/views.py
+++ friendship/views.py
@@ -20,7 +20,7 @@ class IndexView(generic.ListView):
         return UserInfo.objects.all()
 
 
-class DetailsView(generic.DetailsView):
+class DetailsView(generic.DetailView):
     model = UserInfo
     template_name = 'friendship/details.html'
 
