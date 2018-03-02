dbf8853281dc7b181698529004bd279fa206b625
Hansung Kang <h.k@duke.edu>
2018-03-01 20:23:26 -0500

    registration trial

--- friendship/views.py
+++ friendship/views.py
@@ -26,7 +26,7 @@ class IndexView(generic.ListView):
 
 
 def details(request, id):
-    return render(request, 'friendship/details.html', {'userinfo': get_object_or_404(User, pk=id)})
+    return render(request, 'friendship/details.html', {'userinfo': get_object_or_404(UserInfo, pk=id)})
 
 
 def register(request):
