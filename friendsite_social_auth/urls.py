cd8f6362fd0b9ecfb98b5c59cf12b806196bb576
Hansung Kang <han950307@gmail.com>
2018-03-31 03:41:06 -0400

    making fb loging now

--- friendsite_social_auth/urls.py
+++ friendsite_social_auth/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-	path('facebook_login/', views.facebook_login, name="facebook_login")
+	path('facebook_login/', views.facebook_login, name="facebook_login"),
     path('facebook_callback/', views.facebook_callback, name="facebook_callback"),
 ]
