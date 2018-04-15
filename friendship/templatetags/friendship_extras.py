339984c5929a3b214327f58c4a213f60beb22be2
Hansung Kang <han950307@gmail.com>
2018-04-15 18:40:24 -0400

    added current lowest bid amount

--- friendship/templatetags/friendship_extras.py
+++ friendship/templatetags/friendship_extras.py
@@ -1,5 +1,5 @@
 from django import template
-from friendship.models import Money
+from friendship.models import Money, Bid
 
 import math
 
@@ -18,3 +18,19 @@ def get_money_str(money, currency):
     if not money:
         return None
     return money.get_value_str(currency)
+
+
+@register.simple_tag
+def get_lowest_bid_str(order, currency):
+	bids = Bid.objects.filter(order=order)
+	lowest_bid = None
+	lowest_val = None
+	for bid in bids:
+		val = bid.get_total()
+		if not lowest_val or val < lowest_val:
+			lowest_bid = bid
+			lowest_val = val
+
+	if lowest_bid:
+		return lowest_bid.get_total_str(currency)
+	return None
