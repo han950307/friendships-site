eef3c2ddb0387538924d831502e4c598892ef839
Hansung Kang <han950307@gmail.com>
2018-04-23 00:57:17 -0400

    added thai to rendering

--- marketing_manager/forms.py
+++ marketing_manager/forms.py
@@ -5,10 +5,17 @@ from marketing_manager.models import FAQ
 class FAQEditForm(forms.ModelForm):
     class Meta:
         model = FAQ
-        fields = ['inner_HTML']
+        fields = ['inner_HTML', 'inner_HTML_thai']
 
         widgets = {
             'inner_HTML': forms.Textarea(
+                attrs={
+                    'rows': 20,
+                    'placeholder': 'Name*',
+                    'class': 'input form-control',
+                }
+            ),
+            'inner_HTML_thai': forms.Textarea(
                 attrs={
                     'rows': 20,
                     'placeholder': 'Name*',
