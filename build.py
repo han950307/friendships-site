b3627149b0f3cf4b491fde1bc4acf8a5b2e96fcb
Hansung Kang <han950307@gmail.com>
2020-08-04 17:26:15 -0400

    prod

---
+++ build.py
@@ -0,0 +1,39 @@
+import re
+import shlex
+import subprocess
+import sys
+import traceback
+
+
+def run_command(command):
+    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+    while True:
+        output = process.stdout.readline()
+        if output == "" and process.poll() is not None:
+            break
+        elif output:
+            print(output.strip().decode("utf-8"))
+        else:
+            break
+    rc = process.poll()
+    return rc
+
+
+with open("friendsite/production_secrets.py") as f:
+    for line in f:
+        if line.startswith("VERSION"):
+            version = re.search(r"\"(.*)\"", line).group(1)
+            break
+
+try:
+    run_command(
+        "docker build -t friendships/friendships-prod:{} .".format(
+            version
+        )
+    )
+    run_command(
+        "docker push friendships/friendships-prod:{}".format(version)
+    )
+except Exception as e:
+    print(e)
+    traceback.print_exc()
