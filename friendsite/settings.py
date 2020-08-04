e95529ba6101c5631e64b321e3b1e462d4064c09
Hansung Kang <han950307@gmail.com>
2020-08-04 19:26:58 -0400

    added hosts

--- friendsite/settings.py
+++ friendsite/settings.py
@@ -16,14 +16,15 @@ import friendsite
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-FRIENDSHIP_VERSION = "0.6.0"
+FRIENDSHIP_VERSION = "0.6.1"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # WSGI_APPLICATION = "friendsite.wsgi.application"
 ALLOWED_HOSTS = [
-    "172.31.95.146",  # Load Balancer
+    "35.171.192.47",
+    "172.31.66.51",  # Load Balancer
     "18.188.123.79",
     "127.0.0.1",
     "dev.friendships.link",
